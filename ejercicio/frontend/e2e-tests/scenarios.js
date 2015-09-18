0e5deaf12bef06ec90abe935e18ac52d35f369a7
Rodolfo Campos <camposer@gmail.com>
2015-09-18 14:03:38 +0200

    Implementación de la prueba e2e de agregar producto

--- ejercicio/frontend/e2e-tests/scenarios.js
+++ ejercicio/frontend/e2e-tests/scenarios.js
@@ -18,13 +18,37 @@ describe('Pruebas de aplicación', function() {
     });
 
 
-    it('Debería mostrar la el títulos de productos', function() {
+    it('Debería mostrar el títulos de productos', function() {
       expect(element.all(by.css('h1')).first().getText()).
         toMatch(/Productos/);
     });
 
-    it('Debería mostrar la el títulos de productos', function() {
-      expect(element.all(by.repeater('p in ctrl.productos')).count()).toEqual(3);
+    it('Debería mostrar la lista de productos', function() {
+      expect(element.all(by.repeater('p in ctrl.productos')).count()).toBeGreaterThan(0);
+    });
+
+    it('Debería agregar producto', function() {
+      element(by.model('ctrl.producto.nombre')).sendKeys('nuevo');
+      element(by.model('ctrl.producto.precio')).sendKeys('200');
+
+      var tamanoInicial;
+
+      element
+        .all(by.repeater('p in ctrl.productos'))
+        .count()
+        .then(function(valor) {
+          tamanoInicial = valor;
+        });
+
+        element(by.css('button[type=submit]')).click();
+          
+        element
+            .all(by.repeater('p in ctrl.productos'))
+            .count()
+            .then(function(valor) {
+              expect(valor).toEqual(tamanoInicial + 1);
+            });
+
     });
 
   });
